c276205fabda87c8d7b9f8cbfe9487abb0350b71
Abdalrahman Samir <abdalr20088002@gmail.com>
2025-02-09 15:08:47 +0200

    Add files via upload

---
+++ Even_or_Odd.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+
+    if(x % 2 == 0)
+        cout << "This number is Even";
+    else
+        cout << "This number is Odd";
+}
